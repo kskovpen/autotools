ce8fc637f2b83545dc2fc8859f49a79f32ef6c3b
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2025-07-26 13:30:16 +0200

    update

--- getall.sh
+++ getall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 curl -L https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.gz | tar xzvvf -
 curl -L http://ftpmirror.gnu.org/libtool/libtool-2.4.7.tar.gz | tar xzvvf -
-curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.70.tar.gz | tar xzvvf -
 curl -L http://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2016.03.20.tar.xz | tar xJf -
 curl -L http://ftp.gnu.org/pub/gnu/gettext/gettext-0.20.1.tar.gz | tar xzvvf -
 curl -L http://ftp.gnu.org/gnu/automake/automake-1.16.2.tar.gz | tar xzvvf -
